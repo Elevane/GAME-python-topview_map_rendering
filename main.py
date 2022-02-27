518c80a4b92042452071e11c10dc4b68486b5428
bastien aubry <pma.bastien.aubry@gmail.com>
2022-02-27 11:45:55 +0100

    class player : map

--- main.py
+++ main.py
@@ -1,23 +1,88 @@
 
 import pygame , sys
 
-MAP = [0 for i in range(20) ]
+MAP = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+
+class Map:
+    def __init__(self, game):
+        self.map = MAP
+        self.game = game
+    
+    def draw(self):
+        if self.map:
+            for i, row in enumerate(self.map):
+                for y, col in enumerate(row):
+                    if col == 1:
+                        img = pygame.image.load("img\grass_block.png").convert_alpha()
+                        self.game.screen.blit(img, (y*32, i*32))
+
+    def collide_tile(self, pos):
+        for i, row in enumerate(self.map):
+            for y, col in enumerate(row):
+                collider = pygame.Rect(y* 32, i*32, 32, 32)
+                if collider.collidepoint(pos):
+                    return True
+        return False
+                    
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x = 200
+        self.y = 200
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen,(255,0,0), (self.x, self.y, 20, 50))
+
+    def update(self):
+        if self.game.map.collide_tile((self.x, self.y)):
+            self.y += 2
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 640))
         pygame.display.set_caption("test sprites")
         self.clock = pygame.time.Clock()
+        self.map = Map(self)
+        self.player = Player(self)
 
     def run(self):
         while True:
             self.dt = self.clock.tick(60) / 1000
             self.update()
             self.events()
+            self.draw()
+
+    def draw(self):
+        self.map.draw()
+        self.player.draw()
 
     def update(self):
         pygame.display.update()
         self.screen.fill((0, 0, 0))
+        self.player.update()
 
     def events(self):
         for event in pygame.event.get():
